ccf7f1e70f3dacfe325907bc40a39fd9e5b86a60
joerodgers <28455042+joerodgers@users.noreply.github.com>
2023-08-07 17:05:51 -0400

    added identity column to audit table

--- src/Jobs/Invoke-SiteCertificationExpiredSiteNoAccessLock.ps1
+++ src/Jobs/Invoke-SiteCertificationExpiredSiteNoAccessLock.ps1
@@ -1,1 +1,49 @@
 ﻿
+
+#requires -Version 7.2
+
+$ErrorActionPreference = 'Stop'
+
+Import-Module -Name "PnP.PowerShell"           -MinimumVersion "2.2.0"   -Force -ErrorAction Stop
+Import-Module -Name "PSFramework"              -MinimumVersion "1.8.291" -Force -ErrorAction Stop
+Import-Module -Name "Tenant.SiteCertification" -MinimumVersion "1.0.0"   -Force -ErrorAction Stop
+
+[System.Net.ServicePointManager]::SecurityProtocol     = [System.Net.SecurityProtocolType]::Tls12
+[System.Net.Http.HttpClient]::DefaultProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+
+$timestamp = Get-Date -Format FileDateTime
+
+Start-SiteCertificationLogFileLogger -FilePath "C:\_temp\logs\site-noaccesslock-_$timestamp.csv"
+
+$tc = New-SiteCertificationTenantConnection `
+        -ClientId              $env:O365_CLIENTID `
+        -CertificateThumbprint $env:O365_THUMBPRINT `
+        -TenantName            "$($env:O365_TENANT).onmicrosoft.com" `
+        -TenantId              $env:O365_TENANTID
+
+$dc = New-SiteCertificationDatabaseConnection `
+        -DatabaseName          "sql-dataconnect" `
+        -DatabaseServer        "srv-dataconnect.database.windows.net" `
+        -ClientId              "a5f805f0-6c3c-483e-9bb8-53c65ea299e1" `
+        -CertificateThumbprint $env:O365_THUMBPRINT `
+        -TenantId              "ef74271e-fef6-44f2-802c-7142413a35c6"
+
+$cfg = New-SiteCertificationConfiguration `
+        -ExecutionDate                   ([DateTime]::Today) `
+        -VerificationIntervalDays        90 `
+        -NotificationFrequencyDays       7 `
+        -NotificationsBeforeNoAccessLock 6 `
+        -LockedDaysBeforeDeletion        45 `
+        -ErrorAction                     Stop   
+
+Connect-SiteCertificationService `
+        -DatabaseConnection $dc `
+        -TenantConnection   $tc `
+        -Configuration      $cfg `
+        -ErrorAction        Stop
+
+
+# xx
+                                                  
+Stop-SiteCertificationLogFileLogger
+
--- src/Tenant.SiteCertification/private/sql/table.SiteCollectionAudit.sql
+++ src/Tenant.SiteCertification/private/sql/table.SiteCollectionAudit.sql
@@ -2,6 +2,7 @@
 BEGIN
     CREATE TABLE sitecertification.SiteCollectionAudit
     (
+        Id         int            IDENTITY(1,1),
         SiteUrl    nvarchar(500)  NOT NULL,
         AuditEvent nvarchar(400)  NOT NULL,
         RowCreated datetime2(2)   NOT NULL
